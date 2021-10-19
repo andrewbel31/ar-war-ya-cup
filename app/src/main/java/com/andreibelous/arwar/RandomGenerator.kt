d1a7dfb06f344100f2454d9fb64d86afe43e10c5
andrey <andreyrabo@mail.ru>
2021-10-19 19:47:59 +0300

    bugfix

--- app/src/main/java/com/andreibelous/arwar/RandomGenerator.kt
+++ app/src/main/java/com/andreibelous/arwar/RandomGenerator.kt
@@ -4,7 +4,8 @@ import kotlin.random.Random
 
 object RandomGenerator {
 
-    fun generateSessionId(): Int = Random.nextInt(0, 999_999)
+    fun generateSessionId(): Int = Random.nextInt(100_000, 999_999)
+//    fun generateSessionId(): Int = 111_111
 
     fun generateUserId(): Int = Random.nextInt(0, 99_999)
 }
